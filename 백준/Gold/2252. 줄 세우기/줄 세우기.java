193813fafc5a7f12cd4aa13550703d0ca3b7a1de
Park Si Hyeong <108376463+psihyeong@users.noreply.github.com>
2023-06-07 15:37:28 +0900

    [Gold III] Title: 줄 세우기, Time: 1472 ms, Memory: 181320 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -13,31 +13,32 @@ public class Main {
         for (int i = 0; i<=N; i++) {
             A.add(new ArrayList<>());
         }
-        
+
         int indegree[] = new int[N+1];
-        
+
         for (int i=0; i<M; i++) {
             int S = sc.nextInt();
             int E = sc.nextInt();
             A.get(S).add(E);
             indegree[E]++;          // 진입차수 배열 데이터 저장 부분
         }
-        
+
         // 위상정렬
         Queue<Integer> queue = new LinkedList<>();
         
+        // 최초 진입차수가 0이 되는 노드를 큐에 삽입
         for (int i=1; i<=N; i++) {
             if (indegree[i]==0) {
                 queue.offer(i);
             }
         }
-        
+
         while (!queue.isEmpty()) {
-            int now = queue.poll();
+            int now = queue.poll();         // 큐에서 노드를 꺼내고
             System.out.print(now + " ");
-            for (int next : A.get(now)) {
-                indegree[next]--;
-                if (indegree[next]==0) {
+            for (int next : A.get(now)) {   // 꺼낸 노드와 연결되어 있는 간선들을
+                indegree[next]--;           // 제거하고 진입차수 최신화
+                if (indegree[next]==0) {    // 진입차수가 0이 되는 노드를 큐에 삽입
                     queue.offer(next);
                 }
             }
